e9de67e6fa574b13501db50c2b430b96deafe6c2
MD Shafiul Alam Sagar <m.shafiulalamsagar@gmail.com>
2025-03-20 23:04:16 +0600

    Add files via upload

---
+++ bomb.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+m = 30
+time.sleep(5)
+while m>0:
+    pyautogui.typewrite("tob tobi tob tob tobi tob tob tobi tob tobtobali")
+    pyautogui.press("enter")
+    time.sleep(3)
+    m = m-1
